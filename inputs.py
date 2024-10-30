e9c4d3d19eb7955b595cdea29b266841a81ce652
jackomosh <jackomosh6@gmail.com>
2024-10-31 00:58:47 +0300

    get_user_input

---
+++ inputs.py
@@ -0,0 +1,9 @@
+# take user input
+
+# a program that asks the user for their name, age, and location and then prints out a personalized message
+
+name = input('What is your name?: ')
+age = input('How old are you?: ')
+location = input('Which country are you from?: ')
+
+print('Hello', name, 'from', location, 'you are', age, 'years old.')
